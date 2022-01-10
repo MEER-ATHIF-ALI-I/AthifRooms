5aeb26075d5c52a11e6a05f95e2891a7f48c2f65
MEER ATHIF ALI I <meerathifali@gmail.com>
2022-01-10 21:54:09 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+var mongoURL = process.env.MONGODB_URL
+
+mongoose.connect( mongoURL , {useUnifiedTopology:true , useNewUrlParser:true})
+
+var dbconnect = mongoose.connection
+
+dbconnect.on('error' , ()=>{
+    console.log(`Mongo DB Connection Failed`);
+})
+
+dbconnect.on('connected' , ()=>{
+    console.log(`Mongo DB Connection Successfull`);
+})
+
+module.exports = mongoose
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const db=require('./db')
+app.use(express.json())
+const path = require('path')
+const roomsRoutes = require('./routes/roomsRoute')
+const userRoute = require('./routes/userRoute')
+const bookingsRoute=require('./routes/bookingsRoute')
+app.use('/api/rooms',roomsRoutes)
+app.use('/api/users' , userRoute)
+app.use('/api/bookings' , bookingsRoute)
+
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Node JS Server Started`));
