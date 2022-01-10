4b556e09c40021180cea79fdc2060eb624df3080
MEER ATHIF ALI I <meerathifali@gmail.com>
2022-01-10 22:01:39 +0530

    scripts added

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,24 @@ app.use('/api/rooms',roomsRoutes)
 app.use('/api/users' , userRoute)
 app.use('/api/bookings' , bookingsRoute)
 
+//----------deployment-----------------
 
+__dirname = path.resolve();
+
+if(process.env.NODE_ENV==="production") {
+ app.use(express.static(path.join(__dirname,"/frontend/build")));
+
+ app.get('*',(req,res)=>{
+   res.sendFile(path.resolve(__dirname,"frontend","build","index.html"));
+ });
+}else{
+	app.get("/", (req, res)=>{
+      res.send("API is running..");
+	});
+}
+
+
+//----------deployment-----------
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Node JS Server Started`));
